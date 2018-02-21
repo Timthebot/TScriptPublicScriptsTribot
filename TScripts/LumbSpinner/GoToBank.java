87e3d30a8245618b757e112d18bc533c4610e7a4
Tim Lehner <Timothy.lehner.2011@live.rhul.ac.uk>
2018-02-21 21:15:45 +0000

    Cleaned up
    Don't know how to check if right click is open
    Also removed unnecesary system.out message

--- TScripts/LumbSpinner/GoToBank.java
+++ TScripts/LumbSpinner/GoToBank.java
@@ -1,11 +1,16 @@
 package scripts.TScripts.LumbSpinner;
 
+import org.tribot.api.Clicking;
+import org.tribot.api.DynamicClicking;
 import org.tribot.api.General;
 import org.tribot.api.Timing;
+import org.tribot.api.input.Mouse;
 import org.tribot.api.types.generic.Condition;
 import org.tribot.api.util.abc.ABCUtil;
+import org.tribot.api2007.Interfaces;
 import org.tribot.api2007.Inventory;
 import org.tribot.api2007.Objects;
+import org.tribot.api2007.Options;
 import org.tribot.api2007.types.RSItem;
 import org.tribot.api2007.types.RSObject;
 
@@ -24,16 +29,11 @@ public class GoToBank extends TSpinNode {
     public void execute() {
         openDoorIfNeeded();
 
-        if (false ){//&& hovering) {
-            // click climb-up
-
-        } else {
-            RSObject[] stairs = Objects.findNearest(15, "Staircase");
-            if (stairs.length > 0) {
-                stairs[0].click("Climb-up");
-                idle();
-                Timing.waitCondition(isOnTopFloor, General.random(3000, 7000));
-            }
+        RSObject[] stairs = Objects.findNearest(15, "Staircase");
+        if (stairs.length > 0) {
+            stairs[0].click("Climb-up");
+            idle();
+            Timing.waitCondition(isOnTopFloor, General.random(3000, 7000));
         }
     }
 }
